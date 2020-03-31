dc431ef3539bfea2ad2633f918138ba40c676e9a
maasa1221 <maasa1221@gmail.com>
2020-03-31 16:59:15 +0900

    list update

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -2,45 +2,40 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 import routes from '../constants/routes.json';
 import styles from './Home.css';
-import fs from 'fs'
-import styled from "styled-components"
+import fs from 'fs';
+import styled from 'styled-components';
 
 const StyledDiv = styled.div`
-  width: 10rem;
+  width: 100%;
   display: block;
-  `;
+`;
 
-export const Home:React.FC = ( ) => {
-  const [isDir, setIsDir] = React.useState([""]);
+export const Home: React.FC = () => {
+  const [isDir, setIsDir] = React.useState(['']);
   const [isLoad, setIsLoad] = React.useState(true);
-  while(isLoad){
-  fs.readdir(".", (err, dir) => {
-    // es6
-    for(let filePath of dir) {
-      console.log(filePath);
-      setIsDir(dir)
-    }
-    setIsLoad(false)
-  });
-  return (
-    
-      <div className={styles.container} data-tid="container">
-        <h2>CODE MANAGER</h2>
-        <StyledDiv><Link to={routes.COUNTER}>{isDir}</Link></StyledDiv>
-        
-        {/* <input type="file" webkitdirectory></input> */}
+  let dirList = [];
+  while (isLoad) {
+    fs.readdir('/home/hvrl/src/github.com/maasa1221', (err, dir) => {
+      // es6
+      for (let filePath of dir) {
+        console.log(filePath);
+        setIsDir(dir);
+      }
+      setIsLoad(false);
+    });
+    return <></>;
+  }
+  for (let dir of isDir) {
+    dirList.push(
+      <div>
+        <Link to={routes.COUNTER}>{dir}</Link>
       </div>
-    
-    );}
-    return (
-    
-      <div className={styles.container} data-tid="container">
-        <h2>CODE MANAGER</h2>
-        <StyledDiv>
-        <Link to={routes.COUNTER}>{isDir[1]}</Link>
-        <Link to={routes.COUNTER}>{isDir[2]}</Link></StyledDiv>
-        {/* <input type="file" webkitdirectory></input> */}
-      </div>
-    
     );
+  }
+  return (
+    <div className={styles.container} data-tid="container">
+      <h2>CODE MANAGER from ghq</h2>
+      <StyledDiv>{dirList}</StyledDiv>
+    </div>
+  );
 };
